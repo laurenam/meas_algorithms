d4b3e8e7c21acb75a7b8e4b9d57d796aea22d385
Paul Price <price@astro.princeton.edu>
2014-01-20 17:24:09 -0500

    DiscreteBackground: fix bugs
    Need to use PTR(Image) instead of Image&: something nasty happens otherwise
    that leads to segfaults when iterating.
    
    Always work in the "PARENT" frame.

--- src/DiscreteBackground.cc
+++ src/DiscreteBackground.cc
@@ -20,27 +20,26 @@ namespace {
 ///
 /// Takes masked pixels into account
 template <typename T1, typename T2, typename TM>
-double dot(lsst::afw::image::Image<T1> const& image1,
-           lsst::afw::image::Image<T2> const& image2,
-           lsst::afw::image::Mask<TM> const& mask,
-           TM const maskVal             ///< Mask value of pixels to ignore
+double dotProduct(CONST_PTR(lsst::afw::image::Image<T1>) const& image1,
+                  CONST_PTR(lsst::afw::image::Image<T2>) const& image2,
+                  CONST_PTR(lsst::afw::image::Mask<TM>) const& mask,
+                  TM const maskVal             ///< Mask value of pixels to ignore
     )
 {
-    assert(image1.getDimensions() == image2.getDimensions());
-    assert(image1.getDimensions() == mask.getDimensions());
-
+    assert(image1->getDimensions() == image2->getDimensions());
+    assert(image1->getDimensions() == mask->getDimensions());
     double sum = 0.0;
-    typename lsst::afw::image::Image<T1>::const_iterator i1 = image1.begin();
-    typename lsst::afw::image::Image<T2>::const_iterator i2 = image2.begin();
-    typename lsst::afw::image::Mask<TM>::const_iterator m = mask.begin();
-    for (; i1 != image1.end(); ++i1, ++i2, ++m) {
+
+    typename lsst::afw::image::Image<T1>::const_iterator i1 = image1->begin();
+    typename lsst::afw::image::Image<T2>::const_iterator i2 = image2->begin();
+    typename lsst::afw::image::Mask<TM>::const_iterator m = mask->begin();
+    for (; i1 != image1->end() && i2 != image2->end() && m != mask->end(); ++i1, ++i2, ++m) {
         if (!(*m & maskVal)) {
             sum += *i1 * *i2;
         }
     }
     return sum;
 }
-
 
 } // anonymous namespace
 
@@ -57,12 +56,12 @@ struct DiscreteBackground::Impl
                                        boost::make_shared<afw::image::XYTransformFromWcsPair const>(
                                            exp.getWcs(), wcs))),
           solution(),
-          bbox(exp.getBBox()) {
+          bbox(exp.getBBox(afw::image::PARENT)) {
         solve(exp.getMaskedImage(), maskVal);
     }
     Impl(Exposure const& exp, BoxVector const& boxList, WcsVector const& wcsList,
          afw::image::MaskPixel const maskVal)
-        : polygons(), solution(), bbox(exp.getBBox()) {
+        : polygons(), solution(), bbox(exp.getBBox(afw::image::PARENT)) {
         if (boxList.size() != wcsList.size()) {
             throw LSST_EXCEPT(pex::exceptions::LengthErrorException,
                               (boost::format("Box and Wcs length mismatch: %d vs %d") %
@@ -80,7 +79,7 @@ struct DiscreteBackground::Impl
         solve(exp.getMaskedImage(), maskVal);
     }
     Impl(MaskedImage const& image, PolygonVector const& _polygons, afw::image::MaskPixel maskVal)
-        : polygons(_polygons), solution(), bbox(image.getBBox()) {
+        : polygons(_polygons), solution(), bbox(image.getBBox(afw::image::PARENT)) {
         solve(image, maskVal);
     }
     Impl(PolygonVector _polygons, Solution const& _solution, afw::geom::Box2I _bbox)
@@ -104,21 +103,21 @@ void DiscreteBackground::Impl::solve(MaskedImage const& mi, afw::image::MaskPixe
     Matrix matrix = ndarray::allocate(num, num);
     Vector vector = ndarray::allocate(num);
 
-    afw::geom::Box2I const& bbox = mi.getBBox();
-    afw::image::Image<float> const& image = *mi.getImage();
-    afw::image::Mask<afw::image::MaskPixel> const& mask = *mi.getMask();
+    afw::geom::Box2I const& bbox = mi.getBBox(afw::image::PARENT);
+    CONST_PTR(afw::image::Image<float>) const& image = mi.getImage();
+    CONST_PTR(afw::image::Mask<afw::image::MaskPixel>) const& mask = mi.getMask();
 
     // More worried about memory footprint than raw speed, so not caching results of Polygon.createImage().
     // XXX can do much faster dot products by making a subimage of where the polygon(s) are non-zero.
     for (size_t i = 0; i < num; ++i) {
         CartesianPolygon const iPoly = polygons[i];
-        afw::image::Image<float> const& iImage = *iPoly.createImage(bbox);
-        matrix[i][i] = dot(iImage, iImage, mask, maskVal);
-        vector[i] = dot(image, iImage, mask, maskVal);
+        CONST_PTR(afw::image::Image<float>) const& iImage = iPoly.createImage(bbox);
+        matrix[i][i] = dotProduct(iImage, iImage, mask, maskVal);
+        vector[i] = dotProduct(image, iImage, mask, maskVal);
         for (size_t j = i; j < num; ++j) {
             CartesianPolygon const jPoly = polygons[j];
-            afw::image::Image<float> const& jImage = *jPoly.createImage(bbox);
-            matrix[i][j] = matrix[j][i] = dot(iImage, jImage, mask, maskVal);
+            CONST_PTR(afw::image::Image<float>) const& jImage = jPoly.createImage(bbox);
+            matrix[i][j] = matrix[j][i] = dotProduct(iImage, jImage, mask, maskVal);
         }
     }
 
